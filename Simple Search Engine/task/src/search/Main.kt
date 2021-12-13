36b572bc99810bed136040b55d8e3c27c819a007
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2021-12-13 18:41:15 +0300

    Stage 3

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -7,18 +7,41 @@ fun main() {
     println("Enter all people:")
     val allPeople = Array(numPeople) { readLine()!! }
 
-    println("Enter the number of search queries:")
-    val numQueries = readLine()!!.toInt()
+    do {
+        printMenu()
 
-    repeat (numQueries) {
-        println("Enter data to search people:")
-        val query = readLine()!!
-
-        val matches = allPeople.filter { it.contains(query, ignoreCase = true) }
-        if (matches.isEmpty()) {
-            println("No matching people found.")
-        } else {
-            matches.forEach { println(it) }
+        when (readLine()!!.toInt()) {
+            0 -> break
+            1 -> find(allPeople)
+            2 -> print(allPeople)
+            else -> println("Incorrect option! Try again.")
         }
+    } while (true)
+
+    println("Bye!")
+}
+
+fun printMenu() {
+    println("=== Menu ===")
+    println("1. Find a person")
+    println("2. Print all people")
+    println("0. Exit")
+}
+
+fun find(allPeople: Array<String>) {
+    println("Enter a name or email to search all suitable people.")
+    val query = readLine()!!
+
+    val matches = allPeople.filter { it.contains(query, ignoreCase = true) }
+    if (matches.isEmpty()) {
+        println("No matching people found.")
+    } else {
+        matches.forEach { println(it) }
     }
 }
+
+fun print(allPeople: Array<String>) {
+    println("=== List of people ===")
+    allPeople.forEach { println(it) }
+}
+
