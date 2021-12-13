f37000fcbfc35f27d26d3669430e1d2cfee64185
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2021-12-13 18:25:28 +0300

    Stage 2

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,11 +1,24 @@
 package search
 
 fun main() {
-    val input = readLine()!!.split(" ")
-    val a = readLine()!!
-    if (input.indexOf(a) >= 0) {
-        println(input.indexOf(a) + 1)
-    } else {
-        println("Not found")
+    println("Enter the number of people:")
+    val numPeople = readLine()!!.toInt()
+
+    println("Enter all people:")
+    val allPeople = Array(numPeople) { readLine()!! }
+
+    println("Enter the number of search queries:")
+    val numQueries = readLine()!!.toInt()
+
+    repeat (numQueries) {
+        println("Enter data to search people:")
+        val query = readLine()!!
+
+        val matches = allPeople.filter { it.contains(query, ignoreCase = true) }
+        if (matches.isEmpty()) {
+            println("No matching people found.")
+        } else {
+            matches.forEach { println(it) }
+        }
     }
 }
